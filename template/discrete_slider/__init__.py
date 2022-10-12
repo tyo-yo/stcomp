e4a92ed4806b77944994618d96daf8bc3255c515
Tomoaki Nakamura <tyo_yo@icloud.com>
2022-10-12 17:01:34 +0900

    tutorial #2

--- template/discrete_slider/__init__.py
+++ template/discrete_slider/__init__.py
@@ -1,19 +1,26 @@
+import os
+
 import streamlit as st
 import streamlit.components.v1 as components
 
+_RELEASE = True
+
+
+if _RELEASE:
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, "frontend/build")
+    _discrete_slider = components.declare_component("discrete_slider", path=build_dir)
+
+else:
+    _discrete_slider = components.declare_component(
+        "discrete_slider", url="http://localhost:3001"
+    )
+
+
+def discrete_slider(options, key=None):
+    return _discrete_slider(options=options, default="this", key=key)
+
+
 st.title("Component tutorial")
-
-_discrete_slider = components.declare_component(
-    "discrete_slider", url="http://localhost:3001"
-)
-
-
-def discrete_slider(greetings, name="Streamlit", key=None):
-    return _discrete_slider(greetings=greetings, name=name, default=0, key=key)
-
-
-retrun_value = discrete_slider("Hi!", key="one")
-st.write("Number of clicks", retrun_value)
-
-retrun_value = discrete_slider("Hello!", key="two")
-st.write("Number of clicks", retrun_value)
+retrun_value = discrete_slider(["this", "is", "a", "pen"], key="two")
+st.write("you selected:", retrun_value)
